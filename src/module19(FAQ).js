45f2938957b17200a2d09a94bffe26ba55e17266
g-anka <76132583+g-anka@users.noreply.github.com>
2021-08-18 23:22:24 +0300

    Add files via upload

---
+++ src/module19(FAQ).js
@@ -0,0 +1,16 @@
+import "./style(FAQ).css";
+import "./style(About_us).css";
+
+let coll = document.getElementsByClassName("main__questions-btn");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
